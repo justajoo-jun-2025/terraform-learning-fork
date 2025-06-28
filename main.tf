a2d8c72deb51a07fd9e7f72fe578dda744459540
justajoo0374 <justajoo0374@gmail.com>
2025-06-28 23:14:14 +0530

    firsts file vpc with hcp

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      resource = "hashicorp/google"
+      version = "6.41.0"
+      }
+    }
+}
+
+provider "google" {
+  project = "white-resolver-460502-v9"
+}
+
+
+resource "google_compute_network"  "example" {
+  name = "test-vpc-hcp"
+  auto_create_subnetworks = false
+  }
