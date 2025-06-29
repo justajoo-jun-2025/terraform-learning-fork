9147fa0b9b0b2e751409fc9d86f306499ee5a363
justajoo0374 <justajoo0374@gmail.com>
2025-06-29 13:17:09 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,6 +1,6 @@
 provider "google" {
     project = "white-resolver-460502-v9"
-    credentials = file("C/Users/LENOVO/Downloads/white-resolver-460502-v9-cf04ad110a04.json")
+    #credentials = file("C/Users/LENOVO/Downloads/white-resolver-460502-v9-cf04ad110a04.json")
 }
 
 resource "google_compute_network"  "example" {
