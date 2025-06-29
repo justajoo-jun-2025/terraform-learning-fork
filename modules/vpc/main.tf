1c1bb7eb2af1b59d5e9c6033bafa80e0af6f8381
justajoo0374 <justajoo0374@gmail.com>
2025-06-29 13:33:05 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,7 +1,7 @@
-provider "google" {
+/*provider "google" {
     project = "white-resolver-460502-v9"
     #credentials = file("C/Users/LENOVO/Downloads/white-resolver-460502-v9-cf04ad110a04.json")
-}
+}*/
 
 resource "google_compute_network"  "example" {
   name = "test-vpc-hcp"
