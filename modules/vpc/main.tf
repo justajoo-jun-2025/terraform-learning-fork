e2aaacb399b820040127109c91416b2dd3ea22b5
justajoo0374 <justajoo0374@gmail.com>
2025-06-29 13:09:29 +0530

    added

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,2 +1,9 @@
 provider "google" {
-  
+    project = "white-resolver-460502-v9"
+    credentials = file("C/Users/LENOVO/Downloads/white-resolver-460502-v9-cf04ad110a04.json")
+}
+
+resource "google_compute_network"  "example" {
+  name = "test-vpc-hcp"
+  auto_create_subnetworks = false
+  }
